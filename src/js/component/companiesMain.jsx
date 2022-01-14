daa7ce7a5a9fbcb58e2cc8be2b1f0f0919c7c273
AndrxsAF <andrxsaf@gmail.com>
2022-01-14 13:03:51 +0000

    feat "body"

--- src/js/component/companiesMain.jsx
+++ src/js/component/companiesMain.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "../../styles/companiesMain.css";
+import MainCompaniesBg from "../../img/HP-U4B-NYC-bkg.png";
 
 const CompaniesMain = () => {
 	return (
-		<div>
-			<h1>companies</h1>
+		<div
+			style={{
+				backgroundImage: `URL(${MainCompaniesBg})`,
+			}}
+			className="container-fluid companies-container m-0">
+			<p className="custom-fs-safety">Uber para empresas</p>
+			<p>Revoluciona los desplazamientos y cómo comen tus empleados.</p>
+			<button
+				type="button"
+				className="btn btn-dark btn-companies-custom btn-lg fs-6 mt-4">
+				Averigua cómo
+			</button>
 		</div>
 	);
 };
